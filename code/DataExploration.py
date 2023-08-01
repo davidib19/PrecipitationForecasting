1a7070feff33a59bcbbec146648fdaa2242381f4
davidib19 <david.cordova@ib.edu.ar>
2023-08-01 10:35:50 -0500

    A function to merge data and a new figure

--- code/DataExploration.py
+++ code/DataExploration.py
@@ -2,9 +2,9 @@ import xarray
 import os
 import matplotlib.pyplot as plt
 import _pickle as pickle
-import xrscipy.other.signal as dsp
 import numpy as np
 from scipy import signal
+
 maxLat = -3.5
 minLat = -1.5
 minLon = 280.
@@ -13,30 +13,19 @@ lonCajas = 280.75
 latCajas = -2.85
 # Load data
 os.chdir('..')
-"""
-ecu = xarray.open_dataset(os.path.join('data', 'NOAA', 'AirTemperature', '1979.nc'), engine='netcdf4').sel(lat=slice(minLat, maxLat), lon=slice(minLon, maxLon))
-for filename in os.listdir(os.path.join('data', 'NOAA', 'AirTemperature')):
-    if filename.endswith(".nc") and filename != '1979.nc':
-        ecu = xarray.merge([ecu, xarray.open_dataset(os.path.join('data', 'NOAA', 'AirTemperature', filename), engine='netcdf4').sel(lat=slice(minLat, maxLat), lon=slice(minLon, maxLon))])
-#pickle ecu
-print(ecu.lat.values)
-pickle.dump(ecu, open(os.path.join('data', 'NOAA', 'AirTemperature', 'ecuador.p'), 'wb'))
 
-cajas = ecu.sel(lat=latCajas, lon=lonCajas, method='nearest')
-#pickle cajas
-pickle.dump(cajas, open(os.path.join('data', 'NOAA', 'AirTemperature', 'cajas.p'), 'wb'))
-plt.plot(cajas.time, cajas.air)
-plt.show()
-"""
-with open(os.path.join('data', 'NOAA', 'AirTemperature', 'cajas.p'), 'rb') as f:
-    cajas = pickle.load(f)
-T= cajas.air.values.flatten()
-print(T)
-fs=1
-f, t, Sxx = signal.spectrogram(T, fs,nperseg=10*365,noverlap=365)
-f=f*365
-t=t/365
-plt.pcolormesh(t, f, Sxx, shading='gouraud')
-plt.ylabel('Frequency [1/year]')
-plt.xlabel('Time [year]')
-plt.show()
+
+def merger(product, variable, name):
+    """"Junta todos los archivos de la carpeta data/product/variable en un solo archivo llamado name"""
+    files = os.listdir(os.path.join('data', product, variable))
+    data = xarray.open_dataset(os.path.join('data', product, variable, files[0]), engine='netcdf4')
+    for file in files[1:]:
+        data = xarray.merge(
+            [data, xarray.open_dataset(os.path.join('data', product, variable, file), engine='netcdf4')])
+    data.to_netcfd(os.path.join('data', product, variable, name), engine='netcdf4')
+
+
+data = xarray.open_dataset(os.path.join('data', 'ERA5', 'Wind', '2012-2016.nc'), engine='netcdf4')
+data=xarray.merge([data,xarray.open_dataset(os.path.join('data', 'ERA5', 'Wind', '2017-2022.nc'), engine='netcdf4')])
+data=xarray.merge([data,xarray.open_dataset(os.path.join('data', 'ERA5', 'AirTemperature', '2012-2022.nc'), engine='netcdf4')])
+print(data)
