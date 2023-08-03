933f4c4ca2338a1abfe5111a42895a5e8f56d6e8
davidib19 <david.cordova@ib.edu.ar>
2023-08-03 18:03:19 -0500

    Advances in the code

---
+++ code/CNN.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+#creo que voy a tener que usar una rcnn no una cnn
--- code/DataExploration.py
+++ code/DataExploration.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 import _pickle as pickle
 import numpy as np
 from scipy import signal
+import random
 
-maxLat = -3.5
-minLat = -1.5
-minLon = 280.
-maxLon = 288.
+# Constants
 lonCajas = 280.75
 latCajas = -2.85
-# Load data
+latc = 1
+lonc = 1
 os.chdir('..')
 
 
@@ -25,7 +24,54 @@ def merger(product, variable, name):
     data.to_netcfd(os.path.join('data', product, variable, name), engine='netcdf4')
 
 
-data = xarray.open_dataset(os.path.join('data', 'ERA5', 'Wind', '2012-2016.nc'), engine='netcdf4')
-data=xarray.merge([data,xarray.open_dataset(os.path.join('data', 'ERA5', 'Wind', '2017-2022.nc'), engine='netcdf4')])
-data=xarray.merge([data,xarray.open_dataset(os.path.join('data', 'ERA5', 'AirTemperature', '2012-2022.nc'), engine='netcdf4')])
-print(data)
+def add_month(date):
+    """Agrega un mes a la fecha date"""
+    if date[5:7] == '12':
+        return str(int(date[:4]) + 1) + '-01'
+    else:
+        return date[:5] + str(int(date[5:7]) + 1).zfill(2)
+
+
+def X_y_split(data):
+    """Toma un arreglo de datos y lo separa en X e y"""
+    X = []
+    y = []
+    data = data[:, :int(data.shape[1] / 84) * 84, :, 0]
+    train = np.split(data, data.shape[1] / 84, axis=1)
+    for arr in train:
+        X += [arr[:, :72, :, :]]
+        y += [arr[0, 72:, latc, lonc]]
+    return X, y
+
+
+def randomize_dataset(DATA):
+    X_train = []
+    y_train = []
+    X_test = []
+    y_test = []
+    years = list(range(1978, 2023))
+    months = []
+    for i in range(1, 13):
+        ryears = random.sample(years, 8)
+        aux = list(map(lambda x: str(x) + '-' + str(i).zfill(2), ryears))
+        months += aux
+    months.sort()
+    train = DATA.sel(time=slice('1978-01-01', months[0] + '01')).to_array().to_numpy()
+    dummyX, dummyy = X_y_split(train)
+    X_train += dummyX
+    y_train += dummyy
+    for i in range(len(months)):
+        test = DATA.sel(time=slice(months[i] + '-01', add_month(months[i]) + '-01')).to_array().to_numpy()
+        dummyX, dummyy = X_y_split(test)
+        X_test += dummyX
+        y_test += dummyy
+    for i in range(len(months) - 1):
+        train = DATA.sel(time=slice(add_month(months[i]) + '-01', months[i + 1] + '-01'))
+        dummyX, dummyy = X_y_split(train)
+        X_train += dummyX
+        y_train += dummyy
+    train = DATA.sel(time=slice(add_month(months[-1]) + '-01', '2023-01-01'))
+    dummyX, dummyy = X_y_split(train)
+    X_train += dummyX
+    y_train += dummyy
+    return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
